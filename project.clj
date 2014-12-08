e9cccc9de9bf10a5ab54ea8b6db384f927e39450
gitolite tester <tester@example.com>
2014-12-08 16:59:20 -0500

    bump version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [compojure "1.2.1"]
                  [environ "1.0.0"]
-                 [hivewing-core "0.1.0"]
+                 [hivewing-core "0.1.1"]
                  [http-kit "2.1.16"]
                  [clojure-msgpack "0.1.0-SNAPSHOT"]
                  [digest "1.4.4"]
