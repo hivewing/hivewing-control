008f225ad509723446364e2166b67939956e406a
gitolite tester <tester@example.com>
2014-12-17 15:40:23 -0500

    hivewing control

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [compojure "1.2.1"]
                  [environ "1.0.0"]
-                 [hivewing-core "0.1.1"]
+                 [hivewing-core "0.1.3-SNAPSHOT"]
                  [http-kit "2.1.16"]
                  [clojure-msgpack "0.1.0-SNAPSHOT"]
                  [digest "1.4.4"]
@@ -14,19 +14,16 @@
                  [ring-basic-authentication "1.0.5"]
                  [org.clojure/data.json "0.2.5"]
                  ]
+
   :plugins [[s3-wagon-private "1.1.2"]
             [lein-environ "1.0.0"]]
+
   :repositories [["hivewing-core" {:url "s3p://clojars.hivewing.io/hivewing-core/releases"
                                    :username "AKIAJCSUM5ZFGI7DW5PA"
                                    :passphrase "UcO9VGAaGMRuJZbgZxCiz0XuHmB1J0uvzt7WIlJK"}]]
 
+  :uberjar-name "hivewing-control-%s.uber.jar"
+
   :aot [hivewing-control.core]
   :main hivewing-control.core
   )
-  ;:target-path "target/%s"
-  ;   :hivewing-redis-uri "redis://127.0.0.1:3900/"
-  ;   :hivewing-aws-access-key "123abc",
-  ;   :hivewing-aws-secret-key "123abc",
-  ;   :hivewing-ddb-endpoint   "http://127.0.0.1:3800",
-  ;   :hivewing-ddb-worker-config-table "HivewingWorkerConfiguration.v1"
-  ;   :hivewing-sql-connection-string "//127.0.0.1:5432/hivewing-dev?user=hivewing&password=hivewing"
--- src/hivewing_control/core.clj
+++ src/hivewing_control/core.clj
@@ -1,10 +1,10 @@
 (ns hivewing-control.core
   (:require [hivewing-control.server :refer [app]]
-            [org.httpkit.server :refer [run-server]]))
+            [org.httpkit.server :refer [run-server]])
+  (:gen-class))
 
 (defn -main
   [& args]
-  ;; work around dangerous default behaviour in Clojure
   (let [port 4000]
     (println (str "Starting up on port: " port))
     (run-server app {:port port})))
